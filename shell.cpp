77e3acacd5da0b8bf7cc955f38306fb1939596eb
mtmattqq <mtmattqq@gmail.com>
2024-02-24 09:15:14 +0800

    Add execution time

--- shell.cpp
+++ shell.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <iomanip>
+#include <chrono>
 #include "src/pseudo.h"
 #include "src/color.h"
+
+using time_point = std::chrono::steady_clock::time_point;
 
 void RunShell(std::string file_name) {
     SymbolTable global_symbol_table;
@@ -9,7 +12,11 @@ void RunShell(std::string file_name) {
         std::cout << Color(0x34, 0xD3, 0xDE) << "Pseudo >> " RESET;
         std::string input;
         std::getline(std::cin, input);
+        time_point start{std::chrono::steady_clock::now()};
         std::cout << Run(file_name, input, global_symbol_table) << "\n";
+        time_point end{std::chrono::steady_clock::now()};
+        int64_t time_cost{std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()};
+        std::cout << "Execution time: " << time_cost << " ms\n";
     }
 }
 
